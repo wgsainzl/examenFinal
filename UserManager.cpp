87984f36231d60bccebc1e71a1842494bf58e9d8
mauriciovillalobos05 <maugva05@hotmail.com>
2024-11-26 17:50:33 -0600

    Prueba Creacion Clase

---
+++ UserManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maugv on 26/11/2024.
+//
+
+#include "UserManager.h"
---
+++ UserManager.h
@@ -0,0 +1,14 @@
+//
+// Created by maugv on 26/11/2024.
+//
+
+#ifndef EXAMENFINALLL_USERMANAGER_H
+#define EXAMENFINALLL_USERMANAGER_H
+
+
+class UserManager {
+
+};
+
+
+#endif //EXAMENFINALLL_USERMANAGER_H
