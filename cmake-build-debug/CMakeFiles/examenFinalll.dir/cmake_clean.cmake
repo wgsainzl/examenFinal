2b6f496a98e16081badae0017528e53403f3bc43
dantegzr <a01645480@tec.mx>
2024-11-26 17:53:35 -0600

    menú v1

---
+++ cmake-build-debug/CMakeFiles/examenFinalll.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/examenFinalll.dir/main.cpp.obj"
+  "CMakeFiles/examenFinalll.dir/main.cpp.obj.d"
+  "examenFinalll.exe"
+  "examenFinalll.exe.manifest"
+  "examenFinalll.pdb"
+  "libexamenFinalll.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/examenFinalll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
 
+using namespace std;
+
+void mostrarMenu() {
+    cout << "===== Menu =====" << endl;
+    cout << "1. Crear amistad" << endl;
+    cout << "2. Agregar contenido" << endl;
+    cout << "3. Agregar interés a un usuario" << endl;
+    cout << "4. Ver recomendaciones" << endl;
+    cout << "5. Agregar usuario" << endl;
+    cout << "6. Salir" << endl;
+    cout << "Seleccione una opción: ";
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int opcion;
+
+    do {
+        mostrarMenu();
+        cin >> opcion;
+
+        switch (opcion) {
+        case 1:
+            cout << "Has seleccionado: Crear amistad" << endl;
+            break;
+        case 2:
+            cout << "Has seleccionado: Agregar contenido" << endl;
+            break;
+        case 3:
+            cout << "Has seleccionado: Agregar interés a un usuario" << endl;
+            break;
+        case 4:
+            cout << "Has seleccionado: Ver recomendaciones" << endl;
+            break;
+        case 5:
+            cout << "Has seleccionado: Agregar usuario" << endl;
+            break;
+        case 6:
+            cout << "Saliendo del programa..." << endl;
+            break;
+        default:
+            cout << "Opción no válida. Intente de nuevo." << endl;
+        }
+        cout << endl;
+    } while (opcion != 6);
+
     return 0;
 }
